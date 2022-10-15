e31930921329c63f009af35603ce667945f36a3e
rajib <rjibnasipuri@gmail.com>
2022-10-15 21:13:01 +0530

    navbar fixed

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import {BrowserRouter as Router} from "react-router-dom"
+import './App.css';
+import Header from "./components/layout/Header/Header";
+function App() {
+
+  
+  return (
+    <Router>
+     <Header/>
+    </Router>
+  );
+}
+
+export default App;
